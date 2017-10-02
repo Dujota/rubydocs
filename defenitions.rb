ad991f1a5ee9d52877cdc4c3d2eb91fe8fd2f891
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-02 17:39:47 -0400

    finished trying out extensions

---
+++ defenitions.rb
@@ -0,0 +1,31 @@
+# String
+
+length
+strip
+split
+start_with?
+
+
+# Array
+
+array.first
+array.delete_at
+array.delete
+array.pop
+
+
+# Hash
+
+(1..5)to_a
+has_key?
+has_value?
+
+
+# Time
+Time.now
+
+
+# File
+
+exist?
+extname
